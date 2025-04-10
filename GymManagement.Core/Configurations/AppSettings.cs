cdd08e79e2ac7b17112d98e7b0bf284aa3144baa
guilliammrst <guilliam.morisset85@gmail.com>
2025-04-10 20:25:48 +0200

    fix: add unversionned file

---
+++ GymManagement.Core/Configurations/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace GymManagement.Core.Configurations
+{
+    public static class AppSettings
+    {
+        public const string KeyVaultOptions = "KeyVaultOptions";
+        public const string CachingOptions = "CachingOptions";
+        public const string IssuerOptions = "IssuerOptions";
+    }
+}
